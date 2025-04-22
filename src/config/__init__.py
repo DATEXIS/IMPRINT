cba23daab354d11d62853fe00e7df9840fa1a381
justus.westerhoff <justus.westerhoff@gmail.com>
2025-04-22 15:17:20 +0200

    fix import

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,6 +2,6 @@
 Configuration module for the weight imprinting framework.
 """
 
-from src.config.experiments import get_configurations, clear_configurations
+from src.config.experiments import generate_combinations, filter_combinations
 
 __all__ = ["get_configurations", "clear_configurations"]
